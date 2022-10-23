be7cb4248bf3b4e9f19a3ba81ebd0b0b1b833da6
ron bar yosef <ronibary@gmail.com>
2022-10-23 20:08:03 +0300

    changes from ron in DevClass

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,9 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev class");
         // ron bary added this
+
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
 
 }
