e476de22c12bb960fd73076ed5c40eb227af5b30
ron bar yosef <ronibary@gmail.com>
2022-10-24 18:01:51 +0300

    change from local machine

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,7 +3,7 @@ public class DevClass {
         System.out.println("Dev class");
         // ron bary added this
 
-        System.out.println("Hello world from Feature 1 !!!");
+        System.out.println("hi from roni user1");
     }
 
 }
